8f27cbf3710c823eea5d0ad83849bbe245726631
sujeedo <67086929+sujeedo@users.noreply.github.com>
2020-10-12 21:13:51 +0900

    Add : 로그인 입력폼 송신 이벤트 리스너 js 추가
    해당 사이트를 만들때부터 로그인 기능을 구현하고 싶었습니다.
    
    그래서 '노마드코더 자바스크립트 오픈 강의'를 보며 공부한 로컬스트로지에 값을 저장하는 방법을 응용했습니다.
    
    아직 회원가입 기능을 추가하지 않은 상태인지라,
    회원가입 없이도 로그인을 시키기 위해 체크박스 선택 유무를 조건으로 지정했습니다.
    
    로컬스토리지에 입력값이 저장되면 로그인이 되었다는걸 알리기 위해 저장 된 입력값을 출력합니다.

--- js/loginModal.js
+++ js/loginModal.js
@@ -59,3 +59,34 @@ loginCloseBtn.addEventListener('click', () => {
   loginBox.classList.add('none');
   inputReset();
 });
+
+// 아이디와 패스워드를 입력하고 로그인버튼을 통해 로그인 입력폼 데이터를 송신하면 아래의 코드가 실행됩니다.
+loginInputForm.addEventListener('submit', (event) => {
+  // 송신 이벤트의 기본 동작을 중단시켜줍니다.
+  event.preventDefault(); 
+  // 체크박스 선택 유무를 조건으로 코드를 실행합니다.
+  const currentUser = loginInput.value
+  const checkBox = loginCheckBox.checked;
+  // 만약 체크박스가 선택되어있는 상태라면,
+  // 모달창을 닫습니다. 
+  // 로컬스토리지에 값을 저장 후 저장된 값을 유저 네임란에 출력합니다.
+  // 숨겨두었던 유저 네임란을 다시 보이게 합니다.
+  // 로그인 버튼을 로그아웃 버튼으로 바꿉니다.
+  if(checkBox) {
+    modalContainer.classList.add('none');
+    loginBox.classList.add('none');
+    localStorage.setItem('user', currentUser);
+    let currentUserLS = localStorage.getItem('user');
+    userNameText.innerHTML = `welcome! <span style='color:#49ade7'>${currentUserLS}</span>`;
+    userName.classList.remove('hidden'); 
+    userStatusBtn.innerText = 'LOGOUT';
+  }
+  // 만약 체크되어 있지 않다면 로그인 실패 모달창이 나타납니다.
+  if(checkBox == false) {
+    loginBox.classList.add('none');
+    loginFailBox.classList.remove('none');
+  }
+  // 입력값을 유지할 필요가 없으므로 입력 값을 초기화시켜줍니다.
+  inputReset(); 
+});
+
