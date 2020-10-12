9a4c39a01a693934c983a7bcdf972fa698b0e3c9
sujeedo <67086929+sujeedo@users.noreply.github.com>
2020-10-13 07:28:17 +0900

    Fix : 브레드 크럼 업데이트 중 에러사항 발생 js 수정중
    * 업데이트 사항
    
    현재 스크롤 이벤트가 발생하였을 때와 클릭 이벤트가 발생하였을 때만 사용자의 섹션 위치가 표시됩니다.
    
    현재 사용자가 위치하지 않은 섹션 네임은 브레드크럼에 표시하지 않기 위해 display:none을 준 상태입니다.
    
    마우스가 브레드크럼에 진입하였을때 모든 메뉴가 display:block이 되어 브레드크럼에 표시는 되지만, 현재 위치가 active되지 않아 수정중입니다.
    
    스크롤 이벤트가 발생하지 않아도, 클릭하지 않아도 현재 위치가 표시되도록 수정해야합니다.

--- js/breadcrumb.js
+++ js/breadcrumb.js
@@ -2,6 +2,7 @@
 
 const html = document.querySelector('html');
 const BreadCrumb = document.querySelector('.bread');
+const BreadCrumbTextBoxs = document.querySelectorAll('.bread li');
 const BreadCrumbTexts = document.querySelectorAll('.bread a');
 const banner = document.querySelector('.banner_container');
 const product = document.querySelector('.product');
@@ -12,16 +13,17 @@ const newsletter = document.querySelector('.newsletter');
 const contact = document.querySelector('.contact');
 const footer = document.querySelector('footer');
 
+const bannerH =  Math.floor(window.pageYOffset + banner.getBoundingClientRect().top);
+const productH = Math.floor(window.pageYOffset + product.getBoundingClientRect().top);
+const topicH = Math.floor(window.pageYOffset + topic.getBoundingClientRect().top);
+const searchZoneH = Math.floor(window.pageYOffset + searchZone.getBoundingClientRect().top);
+const recentH = Math.floor(window.pageYOffset + recent.getBoundingClientRect().top);
+const newsletterH = Math.floor(window.pageYOffset + newsletter.getBoundingClientRect().top);
+const contactH = Math.floor(window.pageYOffset + contact.getBoundingClientRect().top);
+const footerH = Math.floor(window.pageYOffset + footer.getBoundingClientRect().top);
+
 window.addEventListener('scroll', () => {
   let hrmlScroll = html.scrollTop;
-  const bannerH =  Math.floor(window.pageYOffset + banner.getBoundingClientRect().top);
-  const productH = Math.floor(window.pageYOffset + product.getBoundingClientRect().top);
-  const topicH = Math.floor(window.pageYOffset + topic.getBoundingClientRect().top);
-  const searchZoneH = Math.floor(window.pageYOffset + searchZone.getBoundingClientRect().top);
-  const recentH = Math.floor(window.pageYOffset + recent.getBoundingClientRect().top);
-  const newsletterH = Math.floor(window.pageYOffset + newsletter.getBoundingClientRect().top);
-  const contactH = Math.floor(window.pageYOffset + contact.getBoundingClientRect().top);
-  const footerH = Math.floor(window.pageYOffset + footer.getBoundingClientRect().top);
   
   // activer 초기화 함수
   function resetActive() {
@@ -30,37 +32,70 @@ window.addEventListener('scroll', () => {
     });
   }
 
+  // display 초기화 함수
+  function resetDisplay() {
+    BreadCrumbTextBoxs.forEach(BreadCrumbTextBox => {
+      BreadCrumbTextBox.classList.remove('display');
+    });
+  }
+
   // 스크롤하여 해당 섹션 영역에 진입하면, 브레드크럼이 나타나 현재 위치를 표시합니다.
   if(bannerH < hrmlScroll && productH > hrmlScroll) {
     BreadCrumb.classList.remove('visible');
     resetActive();
+    resetDisplay();
     BreadCrumbTexts[0].classList.add('active');
+    BreadCrumbTextBoxs[0].classList.add('display');
   }
   if(productH < hrmlScroll && topicH > hrmlScroll) {
     BreadCrumb.classList.add('visible','active');
     resetActive();
+    resetDisplay();
     BreadCrumbTexts[1].classList.add('active');
+    BreadCrumbTextBoxs[1].classList.add('display');
   }
   if(topicH < hrmlScroll && searchZoneH > hrmlScroll) {
     resetActive();
+    resetDisplay();
     BreadCrumbTexts[2].classList.add('active');
+    BreadCrumbTextBoxs[2].classList.add('display');
   }
   if(searchZoneH < hrmlScroll && recentH > hrmlScroll) {
     resetActive();
+    resetDisplay();
     BreadCrumbTexts[3].classList.add('active');
+    BreadCrumbTextBoxs[3].classList.add('display');
   }
   if(recentH < hrmlScroll && newsletterH > hrmlScroll) {
     resetActive();
+    resetDisplay();
     BreadCrumbTexts[4].classList.add('active');
+    BreadCrumbTextBoxs[4].classList.add('display');
   }
   if(newsletterH < hrmlScroll && (contactH - 500) > hrmlScroll) {
     resetActive();
+    resetDisplay();
     BreadCrumbTexts[5].classList.add('active');
+    BreadCrumbTextBoxs[5].classList.add('display');
   }
-  if((contactH - 500) < hrmlScroll && footerH > hrmlScroll) {
+  if((contactH - 500) < hrmlScroll) {
     resetActive();
+    resetDisplay();
     BreadCrumbTexts[6].classList.add('active');
+    BreadCrumbTextBoxs[6].classList.add('display');
   }
+});
+
+BreadCrumb.addEventListener('mouseleave', () => {
+  BreadCrumbTextBoxs.forEach(BreadCrumbTextBox => {
+    BreadCrumbTextBox.classList.remove('display');
+  });
+});
+
+BreadCrumb.addEventListener('mouseenter', () => {
+  BreadCrumbTextBoxs.forEach(BreadCrumbTextBox => {
+    BreadCrumbTextBox.classList.add('display');
+  });
 });
 
 // 클릭하여 해당 섹션 영역으로 이동하면, 현재 위치를 표시합니다.
@@ -70,5 +105,5 @@ BreadCrumb.addEventListener('click', (event) => {
   });
   let targetEl = event.target;
   targetEl.classList.add('active');
-  
+  targetEl.parentNode.classList.add('display');
 });
