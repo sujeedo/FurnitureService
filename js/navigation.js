a358c25aa7c65954c7f60ba5b18a3f5600fe76cc
sujeedo <67086929+sujeedo@users.noreply.github.com>
2020-10-16 09:45:40 +0900

    Fix : 헤더 네비게이션 반응형 이벤트 js 추가 수정 완료 :angel:
    * 수정 사항
    
    브라우저 너비값이 812px 이상이냐 아니냐를 조건으로 if문을 만들어 812px 이하는 네비게이션 이벤트를 클릭에 초점을 두었고, 812px 이상은 마우스 커서 진입에 초점을 두어 구현하였습니다.
    
    과거에 브라우저 창 사이즈 변화를 감지하기 위해 resize이벤트를 사용하여 코드를 작성하였지만 페이지 로드시 이벤트가 작동하지 않았었습니다. 그래서 이번에는 reactive 함수를 만들어 resize이벤트가 발생할 때와 load이벤트가 발생할때 모두 reactive 함수를 호출하도록 하여 로드시 네비게이션의 이벤트가 반응하지 않던 문제를 해결하였습니다.

--- js/navigation.js
+++ js/navigation.js
@@ -10,74 +10,19 @@ const arrowicons = document.querySelectorAll('header .gnb .ico_arrow');
 const subMenuLists = document.querySelectorAll('header .gnb .sub_menu a');
 const searchArea = document.querySelectorAll('.home .search_area select');
 
+/*
+window.onresize = function mobileSize() {};
+if(window.clientWidth > 820px) {};
+window.addEventListener('resize', () => {});
+let media = window.matchMedia('screen and (min-width: 812px)').matches == true
+window.addEventListener('resize', () => {if(media == true) {}});
+*/
+
 // drawer버튼을 클릭하면 gnb가 열립니다. 그리고 drawer모양이 close모양으로 바뀝니다.
 drawerBtn.addEventListener('click', () => {
   menu.classList.toggle('open_gnb');
   drawerBtn.classList.toggle('close_icon');
   userStatusBtn.classList.toggle('left');
-});
-
-// gnb의 상단메뉴 중 하위메뉴가 있는 메뉴를 클릭하면 하위메뉴가 열립니다.
-mainMenuTexts[0].addEventListener('click', () => {
-  subMenus[0].classList.toggle('open_sub_menu');
-  arrowicons[0].classList.toggle('top');
-  subMenus[1].classList.remove('open_sub_menu');
-  subMenus[2].classList.remove('open_sub_menu');
-  arrowicons[1].classList.remove('top');
-  arrowicons[2].classList.remove('top');
-});
-mainMenuTexts[1].addEventListener('click', () => {
-  subMenus[1].classList.toggle('open_sub_menu');
-  arrowicons[1].classList.toggle('top');
-  subMenus[0].classList.remove('open_sub_menu');
-  subMenus[2].classList.remove('open_sub_menu');
-  arrowicons[0].classList.remove('top');
-  arrowicons[2].classList.remove('top');
-});
-mainMenuTexts[2].addEventListener('click', () => {
-      subMenus[2].classList.toggle('open_sub_menu');
-      arrowicons[2].classList.toggle('top');
-      subMenus[0].classList.remove('open_sub_menu');
-      subMenus[1].classList.remove('open_sub_menu');
-      arrowicons[0].classList.remove('top');
-      arrowicons[1].classList.remove('top');
-    });
-
-// gnb의 상단메뉴 중 하위메뉴가 없는 메뉴를 클릭하면 하위메뉴가 닫힙니다.
-mainMenus[3].addEventListener('click', () => {
-  subMenus[0].classList.remove('open_sub_menu');
-  subMenus[1].classList.remove('open_sub_menu');
-  subMenus[2].classList.remove('open_sub_menu');
-  arrowicons[0].classList.remove('top');
-  arrowicons[1].classList.remove('top');
-  arrowicons[2].classList.remove('top');
-});
-
-// 마우스 커서가 하위메뉴를 벗어나면 닫힙니다.
-subMenus[0].addEventListener('mouseleave', () => {
-  subMenus[0].classList.remove('open_sub_menu');
-  arrowicons[0].classList.remove('top');
-});
-subMenus[1].addEventListener('mouseleave', () => {
-  subMenus[1].classList.remove('open_sub_menu');
-  arrowicons[1].classList.remove('top');
-});
-subMenus[2].addEventListener('mouseleave', () => {
-  subMenus[2].classList.remove('open_sub_menu');
-  arrowicons[2].classList.remove('top');
-});
-// focus가 하위메뉴 마지막 요소를 벗어나면 닫힙니다.
-subMenuLists[11].addEventListener('focusout', () => {
-  subMenus[0].classList.remove('open_sub_menu');
-  arrowicons[0].classList.remove('top');
-});
-subMenuLists[17].addEventListener('focusout', () => {
-  subMenus[1].classList.remove('open_sub_menu');
-  arrowicons[1].classList.remove('top');
-});
-subMenuLists[19].addEventListener('focusout', () => {
-  subMenus[2].classList.remove('open_sub_menu');
-  arrowicons[2].classList.remove('top');
 });
 
 // drawer버튼을 클릭하여 gnb영역을 닫습니다.
@@ -103,6 +48,136 @@ menuBg.addEventListener('click', () => {
   drawerBtn.classList.remove('close_icon');
 });
 
+function reactive() {
+  if(window.innerWidth < 812) {
+    console.log('모바일')
+
+    // gnb의 상단메뉴 중 하위메뉴가 있는 메뉴를 클릭하면 하위메뉴가 열립니다.
+    mainMenuTexts[0].addEventListener('click', () => {
+      subMenus[0].classList.toggle('open_sub_menu');
+      arrowicons[0].classList.toggle('top');
+      subMenus[1].classList.remove('open_sub_menu');
+      subMenus[2].classList.remove('open_sub_menu');
+      arrowicons[1].classList.remove('top');
+      arrowicons[2].classList.remove('top');
+    });
+    mainMenuTexts[1].addEventListener('click', () => {
+      subMenus[1].classList.toggle('open_sub_menu');
+      arrowicons[1].classList.toggle('top');
+      subMenus[0].classList.remove('open_sub_menu');
+      subMenus[2].classList.remove('open_sub_menu');
+      arrowicons[0].classList.remove('top');
+      arrowicons[2].classList.remove('top');
+    });
+    mainMenuTexts[2].addEventListener('click', () => {
+      subMenus[2].classList.toggle('open_sub_menu');
+      arrowicons[2].classList.toggle('top');
+      subMenus[0].classList.remove('open_sub_menu');
+      subMenus[1].classList.remove('open_sub_menu');
+      arrowicons[0].classList.remove('top');
+      arrowicons[1].classList.remove('top');
+    });
+
+    // gnb의 상단메뉴 중 하위메뉴가 없는 메뉴를 클릭하면 하위메뉴가 닫힙니다.
+    mainMenus[3].addEventListener('click', () => {
+      subMenus[0].classList.remove('open_sub_menu');
+      subMenus[1].classList.remove('open_sub_menu');
+      subMenus[2].classList.remove('open_sub_menu');
+      arrowicons[0].classList.remove('top');
+      arrowicons[1].classList.remove('top');
+      arrowicons[2].classList.remove('top');
+    });
+
+    // 하위 메뉴 마지막 요소를 포커스가 벗어나면, 하위 메뉴가 닫힙니다.
+    subMenuLists[19].addEventListener('focusout', () => {
+      subMenus[2].classList.remove('open_sub_menu');
+      arrowicons[2].classList.remove('top');
+    });
+
+    const loginBtn = document.querySelector('.user_status');
+    // contact us메뉴를 지나면 포커스가 login으로 이동합니다.
+    mainMenus[6].addEventListener('keydown', (e) => {
+      if (e.keyCode === 9 && !e.shiftKey) {
+        e.preventDefault();
+        loginBtn.focus();
+      }
+    });
+    // login메뉴의 이전 포커스를 contact us메뉴로 지정합니다.
+    loginBtn.addEventListener('keydown', (e) => {
+      if (e.shiftKey && e.keyCode === 9) {
+        e.preventDefault();
+        mainMenus[6].querySelector('.main_menu a').focus();
+      }
+    });
+
+  } else {
+    console.log('피씨')
+
+    // gnb의 상단메뉴 중 하위메뉴가 있는 메뉴에 커서가 진입하면 하위메뉴가 열립니다.
+    mainMenuTexts[0].addEventListener('mouseenter', () => {
+      subMenus[0].classList.add('open_sub_menu');
+      subMenus[1].classList.remove('open_sub_menu');
+      subMenus[2].classList.remove('open_sub_menu');
+    });
+    mainMenuTexts[1].addEventListener('mouseenter', () => {
+      subMenus[1].classList.add('open_sub_menu');
+      subMenus[0].classList.remove('open_sub_menu');
+      subMenus[2].classList.remove('open_sub_menu');
+    });
+    mainMenuTexts[2].addEventListener('mouseenter', () => {
+      subMenus[2].classList.add('open_sub_menu');
+      subMenus[0].classList.remove('open_sub_menu');
+      subMenus[1].classList.remove('open_sub_menu');
+    });
+
+    mainMenuTexts[0].addEventListener('focus', () => {
+      subMenus[0].classList.add('open_sub_menu');
+      subMenus[1].classList.remove('open_sub_menu');
+      subMenus[2].classList.remove('open_sub_menu');
+    });
+    mainMenuTexts[1].addEventListener('focus', () => {
+      subMenus[1].classList.add('open_sub_menu');
+      subMenus[0].classList.remove('open_sub_menu');
+      subMenus[2].classList.remove('open_sub_menu');
+    });
+    mainMenuTexts[2].addEventListener('focus', () => {
+      subMenus[2].classList.add('open_sub_menu');
+      subMenus[0].classList.remove('open_sub_menu');
+      subMenus[1].classList.remove('open_sub_menu');
+    });
+
+    // 마우스 커서가 하위메뉴를 벗어나거나 하위메뉴가 없는 메뉴에 진입하면 닫힙니다.
+    subMenus[0].addEventListener('mouseleave', () => {
+      subMenus[0].classList.remove('open_sub_menu');
+      arrowicons[0].classList.remove('top');
+    });
+    subMenus[1].addEventListener('mouseleave', () => {
+      subMenus[1].classList.remove('open_sub_menu');
+      arrowicons[1].classList.remove('top');
+    });
+    subMenus[2].addEventListener('mouseleave', () => {
+      subMenus[2].classList.remove('open_sub_menu');
+      arrowicons[2].classList.remove('top');
+    });
+    mainMenus[3].addEventListener('mouseenter', () => {
+      subMenus[2].classList.remove('open_sub_menu');
+      arrowicons[2].classList.remove('top');
+    });
+    
+    // focus가 하위메뉴 마지막 요소를 벗어나면 닫힙니다.
+    subMenuLists[19].addEventListener('focusout', () => {
+      subMenus[2].classList.remove('open_sub_menu');
+    });
+  }
+}
+
+window.addEventListener('resize', () => {
+  reactive();
+});
+
+window.addEventListener('load', () => {
+  reactive();
+});
 
 /* Header Search Box*/
 
@@ -131,11 +206,3 @@ searchBtn.addEventListener('focusout', () => {
   });
   drawerBtn.classList.remove('close_icon');
 });
-
-/*
-window.onresize = function mobileSize() {};
-if(window.clientWidth > 820px) {};
-window.addEventListener('resize', () => {});
-let media = window.matchMedia('screen and (min-width: 812px)').matches == true
-window.addEventListener('resize', () => {if(media == true) {}});
-*/
